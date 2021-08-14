7b6a935ec55812ea8489a4e09679de63260439d3
Jukka Ylitalo <juylitalo@gmail.com>
2021-08-14 16:24:56 +0300

    disable register

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::group([
 
 ], function () {
     Route::post('login', [AuthController::class, 'login']);
-    Route::post('register', [AuthController::class, 'register']);
+    //Route::post('register', [AuthController::class, 'register']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::get('user-profile', [AuthController::class, 'userProfile']);
